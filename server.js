5a3f4a3c80998d9ffcbfbc8952178b035b79158e
roldie9 <aroldoreyes9@gmail.com>
2020-04-16 02:31:29 -0500

    server.js created

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+//const logger = require("morgan");
+const mongoose = require("mongoose");
+const path = require("path");
+
+const PORT = process.env.PORT || 3000;
+
+//const User = require(".userModel.js");
+const app = express();
+require("./routes/api-routes")(app);
+require("./routes/html-routes")(app);
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutTracker", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    //useCreateIndex: true
+});
